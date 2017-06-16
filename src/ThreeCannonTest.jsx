eb367aa232a559c92538abf8be1244a44233ff58
Jack L <jack.leng@gmail.com>
2017-06-16 04:57:17 -0700

    continued study thru threecannontest

--- src/ThreeCannonTest.jsx
+++ src/ThreeCannonTest.jsx
@@ -16,14 +16,48 @@ export default class ThreeCannonTest extends React.Component{
 
             Cannon: instantiate and initalize:
                 const world = new CANNON.World()
+                    // world is the physics hub that manages objects and simulation.
+                    
                 const bodies = [] 
                 const meshRefs = []
 
-                const initCannon = () => {
-                    //establish cannon constants; 
-                    // gravity, performance attributes, 
-                    // broadphase..? new CANNON.NaiveBroadphase
+                const initCannon = () => { 
 
+                    //establish world's constants
+
+                        world.quatNormalizeSkip = 8 // perf
+                        world.quatNormalizeFast = true // perf
+
+                        world.gravity.set(0, 0, -9.82) 
+                                // meters per second in Z direction
+                                // the react-three-renderer demo actually has -Y direction..??
+                        world.broadphase = new CANNON.NaiveBroadPhase()
+                                // broadphase algo lets world find colliding body 
+
+                    //GROUND: shape, static mass, and rotation
+                        const groundShape = new CANNON.Plane()
+                        const groundBody = new CANNON.Body({mass: 0, shape: groundShape})
+                        groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+
+                    //make walls for container
+                        //to my knowledge it's like plane, just rotated on different axes...?
+
+                    //define COLLISION SHAPES
+                        //loading manager down the road?
+                        //recur through project JSON, which should carry unique params for colision boxes
+                            // new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5))
+
+                }
+
+                
+
+                initCannon()
+                
+                const timeStep = 1 / 60 
+                    // 1/60th second is the update interval
+                
+                const updatePhysics = () => {
+                    world.step(timeStep) // every timestep, advance one frame...? 
                 }
         */
 
