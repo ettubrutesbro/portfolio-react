b6c637f99cab87d2ffd9489f5919fea241fa79c1
Duck Leng <jack.leng@gmail.com>
2017-06-16 01:26:38 -0700

    mockup file ThreeCannonTest

---
+++ src/ThreeCannonTest.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import React3 from 'react-three-renderer'
+import * as THREE from 'three'
+import 
+
+
+export default class ThreeCannonTest extends React.Component{
+    constructor(props, context){
+        super(props, context)
+
+        /*
+            React3: define constants:
+                fog, light positions, rotation quaternions
+                for ground & camera (unless latter is dynamic)
+
+            Cannon: instantiate and initalize:
+                const world = new CANNON.World()
+                const bodies = [] 
+                const meshRefs = []
+
+                const initCannon = () => {
+                    //establish cannon constants; 
+                    // gravity, performance attributes, 
+                    // broadphase..? new CANNON.NaiveBroadphase
+
+                }
+        */
+
+    }
+}
