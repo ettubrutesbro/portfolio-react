b3cad7efa8e892b8622640ef488f7c3c8bc0e9df
Jack L <jack.leng@gmail.com>
2017-06-28 04:56:56 -0700

    componentized projectgroup

--- src/ThreeOimo.jsx
+++ src/ThreeOimo.jsx
@@ -14,12 +14,14 @@ import {FPSStats} from 'react-stats'
 //my stuff
 import {Debug, ThreePhysicsStore} from './Store'
 import { degs, rads} from './helpers.js'
+import ProjectGroup from './ProjectGroup/ProjectGroup'
 
 @observer export default class ThreeOimoTest extends React.Component{
 
     @observable tempVector = new THREE.Vector2()
     @observable raycast = new THREE.Raycaster()
     @observable intersections = []
+    @observable itemsReady = false
 
     constructor(props, context){
         super(props, context)
@@ -33,48 +35,48 @@ import { degs, rads} from './helpers.js'
         this.cameraPosition = new THREE.Vector3(0, 2, 10)
         this.cameraQuaternion = new THREE.Quaternion()
 
-        const world = canvas.world
-        const sizeConstant = canvas.viewableSizingConstant 
+        const world = physics.world
+        const sizeConstant = physics.viewableSizingConstant 
 
         this.establishConstraints = (reestablish) => {
-            const sizeConstant = canvas.viewableSizingConstant
-            const world = canvas.world
+            const sizeConstant = physics.viewableSizingConstant
+            const world = physics.world
             if(reestablish) this.batchConstraintAction('remove',[])
-            canvas.ground = world.add({
+            physics.ground = world.add({
                 type: 'box',
                 size: [sizeConstant, 10, sizeConstant], 
                 pos: [0, -5, 0], 
                 friction: 0.4,
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll,
+                belongsTo: physics.normalCollisions,
+                collidesWith: physics.collidesWithAll,
             })
-            canvas.wallLeft = world.add({
+            physics.wallLeft = world.add({
                 type: 'box',
                 size: [1,100,sizeConstant],
                 pos: [-(sizeConstant/2), 0, 0],
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll
+                belongsTo: physics.normalCollisions,
+                collidesWith: physics.collidesWithAll
             })
-            canvas.wallRight = world.add({
+            physics.wallRight = world.add({
                 type: 'box',
                 size: [1,100,sizeConstant],
                 pos: [sizeConstant/2, 0, 0],
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll
+                belongsTo: physics.normalCollisions,
+                collidesWith: physics.collidesWithAll
             })
-            canvas.wallBack = world.add({
+            physics.wallBack = world.add({
                 type: 'box',
                 size: [sizeConstant,100,1],
                 pos: [0,0,-3],
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll
+                belongsTo: physics.normalCollisions,
+                collidesWith: physics.collidesWithAll
             })
-            canvas.wallFront = world.add({
+            physics.wallFront = world.add({
                 type: 'box',
                 size: [sizeConstant,100,1],
                 pos: [0,0,.5],
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll
+                belongsTo: physics.normalCollisions,
+                collidesWith: physics.collidesWithAll
             })
             if(reestablish){
                 this.batchConstraintAction('setupMass',[0x2,false])
@@ -83,118 +85,79 @@ import { degs, rads} from './helpers.js'
         }
         this.batchConstraintAction = (funcCall, parameters, equals, newVal) => {
             if(!equals){
-                canvas.ground[funcCall](...parameters)
-                canvas.wallLeft[funcCall](...parameters)
-                canvas.wallRight[funcCall](...parameters)
-                canvas.wallFront[funcCall](...parameters)
-                canvas.wallBack[funcCall](...parameters)
+                physics.ground[funcCall](...parameters)
+                physics.wallLeft[funcCall](...parameters)
+                physics.wallRight[funcCall](...parameters)
+                physics.wallFront[funcCall](...parameters)
+                physics.wallBack[funcCall](...parameters)
             }
             else{
-                canvas.ground[funcCall] = newVal
-                canvas.wallLeft[funcCall] = newVal
-                canvas.wallRight[funcCall] = newVal
-                canvas.wallFront[funcCall] = newVal
-                canvas.wallBack[funcCall] = newVal
+                physics.ground[funcCall] = newVal
+                physics.wallLeft[funcCall] = newVal
+                physics.wallRight[funcCall] = newVal
+                physics.wallFront[funcCall] = newVal
+                physics.wallBack[funcCall] = newVal
             }
         }
         this.establishConstraints()
 
-        canvas.roof = world.add({
+        physics.roof = world.add({
             size: [sizeConstant, 10, sizeConstant],
             pos: [0, 40, 0],
-            belongsTo: canvas.normalCollisions,
-            collidesWith: canvas.collidesWithAll
+            belongsTo: physics.normalCollisions,
+            collidesWith: physics.collidesWithAll
         })
-        canvas.hell = world.add({
+        physics.hell = world.add({
             size: [30, 10, 30],
             pos: [0, -25, 0],
             friction: 1,
-            belongsTo: canvas.normalCollisions,
-            collidesWith: canvas.collidesWithAll
+            belongsTo: physics.normalCollisions,
+            collidesWith: physics.collidesWithAll
         })
 
          this.groundQuaternion = new THREE.Quaternion()
             .setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2)
 
-        Object.defineProperty(canvas.ground, 'belongsTo', {
+        Object.defineProperty(physics.ground, 'belongsTo', {
                 get: function(){ return this.shapes.belongsTo },
                 set: function(newBits){ 
                     this.shapes.belongsTo = newBits 
                 }
             })
 
-        this.wallPositionLeft = new THREE.Vector3().copy(canvas.wallLeft.getPosition())
-        this.wallPositionRight = new THREE.Vector3().copy(canvas.wallRight.getPosition())
-        this.wallPositionBack = new THREE.Vector3().copy(canvas.wallBack.getPosition())
-        this.wallPositionFront = new THREE.Vector3().copy(canvas.wallFront.getPosition())
-
-        props.projects.forEach((project, i)=>{
-            const model = project.physicsModel || {types: ['box'], sizes: [1,1,1], positions: [0,0,0]}
-            const body = {
-                name: project.name,
-                type: model.types,
-                size: model.sizes,
-                posShape: model.positions,
-                density: model.density || 10,
-                restitution: model.restitution || 0.001,
-                //random / programmatic for scene purposes
-                pos: [((Math.random()*sizeConstant)-(sizeConstant/2))*.25, sizeConstant+(i*2), -0.75],
-                rot: [(Math.random()*30)-15, (Math.random()*30)-15, (Math.random()*30)-15],
-                move: true,
-                world: world,
-                belongsTo: canvas.normalCollisions,
-                collidesWith: canvas.collidesWithAll & ~canvas.nonCollisionGroup
-            }
-
-            canvas.bodies[project.name] = canvas.world.add(body)
-
-            canvas.meshes[i] = {
-                position: new THREE.Vector3().copy(canvas.bodies[project.name].getPosition()), 
-                rotation: new THREE.Quaternion().copy(canvas.bodies[project.name].getQuaternion())
-            }
-            if(debug.physicsMeshes){
-                canvas.physicsMeshes[i] = model.types.map((type, it)=>{
-                    const n = it*3
-                    return {
-                        geo: type, 
-                        pos: {x: model.positions[n+0], y: model.positions[n+1], z: model.positions[n+2]},
-                        size: {w: model.sizes[n+0], h: model.sizes[n+1], d: model.sizes[n+2], r: model.sizes[n+0]},
-                        color: model.debugColor || 0x888888
-                    }
-                })
-            }
-        })
+        this.wallPositionLeft = new THREE.Vector3().copy(physics.wallLeft.getPosition())
+        this.wallPositionRight = new THREE.Vector3().copy(physics.wallRight.getPosition())
+        this.wallPositionBack = new THREE.Vector3().copy(physics.wallBack.getPosition())
+        this.wallPositionFront = new THREE.Vector3().copy(physics.wallFront.getPosition())
     }
 
-    // checkRaycast = (event) => {
-    //     const containerSize = this.refs.container.getBoundingClientRect()
-    //     let relativeCoords = (this.tempVector.set(event.clientX, event.clientY)).clone()
-    //         .sub(this.tempVector.set(containerSize.left, containerSize.top))
-    //         .divide(this.tempVector.set(containerSize.width, containerSize.height))
-
-    //     relativeCoords.x = relativeCoords.x * 2 -1
-    //     relativeCoords.y = relativeCoords.y * 2 + 1
-
-    //     this.raycast.setFromCamera(relativeCoords, this.refs.camera)
-    //     console.log(this.raycast.intersectObjects(this.refs.scene, true))
-    // }
+    @action
+    setReady = () => {
+        console.log('item ready')
+        this.itemsReady++
+        if(this.itemsReady === this.props.projects.length){
+            console.log('all items ready')
+        }
+    }
 
     animate = () =>{
-        if(debug.runWorld){
-            canvas.world.step()
-        }
-        const projects = this.props.projects
+        if(debug.runWorld && this.itemsReady === this.props.projects.length){
+            physics.world.step()
+        
+            const projects = this.props.projects
 
-        for(var i = 0; i<this.props.projects.length; i++){
-            const name = projects[i].name
-            if(!canvas.bodies[name].sleeping){
-                canvas.meshes[i].position = new THREE.Vector3().copy(canvas.bodies[name].getPosition())
-                canvas.meshes[i].rotation = new THREE.Quaternion().copy(canvas.bodies[name].getQuaternion())
+            for(var i = 0; i<this.props.projects.length; i++){
+                // console.log(projects[i])
+                const name = projects[i].name
+                if(!physics.bodies[name].sleeping){
+                    physics.groups[i].position = new THREE.Vector3().copy(physics.bodies[name].getPosition())
+                    physics.groups[i].rotation = new THREE.Quaternion().copy(physics.bodies[name].getQuaternion())
+                }
+                
             }
-            
-        }
 
-        TWEEN.update()
+            TWEEN.update()
+        }
     }
 
     testImpulse = (body) => {
@@ -256,17 +219,17 @@ import { degs, rads} from './helpers.js'
     }
 
     phaseConstraints = () => {
-        this.batchConstraintAction('belongsTo', null, true, canvas.nonCollisionGroup)
+        this.batchConstraintAction('belongsTo', null, true, physics.nonCollisionGroup)
         this.batchConstraintAction('setupMass', [0x1, true])
     }
     
     restoreLostBodies = () => {
         //map through all bodies that are below a certain Y coord (-10)
-        const sizeConstant = canvas.viewableSizingConstant
-        const allBodies = Object.keys(canvas.bodies)
+        const sizeConstant = physics.viewableSizingConstant
+        const allBodies = Object.keys(physics.bodies)
 
         allBodies.forEach((key, i) => {
-            const body = canvas.bodies[key] 
+            const body = physics.bodies[key] 
             // console.log(body.getPosition())
             if(body.position.y < -5){
                 console.log(key + ' restored')
@@ -305,38 +268,24 @@ import { degs, rads} from './helpers.js'
         // window.clearTimeout(body.timeOutMovement)
         // body.timeOutMovement = undefined
         this.establishConstraints(true)
-        this.reenablePhysics(canvas.bodies[this.props.store.selectedProject])
+        this.reenablePhysics(physics.bodies[this.props.store.selectedProject])
         this.props.store.selectedProject = null
     }
 
     render(){
 
-        const sizeConstant = canvas.viewableSizingConstant
+        const sizeConstant = physics.viewableSizingConstant
 
-        const projectMeshes = canvas.meshes.map((mesh, i)=>{
+        const projectGroups = this.props.projects.map((project,i)=>{
             return(
-                <group
-                        key = {'project'+i+'group'}
-                        position = {mesh.position}
-                        quaternion = {mesh.rotation}>
-                    {debug.physicsMeshes && 
-                    canvas.physicsMeshes[i].map((pmesh, it)=>{
-                        const geo = pmesh.geo === 'box'? ( <boxGeometry width = {pmesh.size.w} height = {pmesh.size.h} depth = {pmesh.size.d} /> )
-                        : pmesh.geo === 'sphere'? ( <sphereGeometry radius = {pmesh.size.r} widthSegments = {8} heightSegments = {8}/>  )
-                        : <boxGeometry width = {0.1} height = {0.1} depth = {0.1} />
-
-                        return (
-                            <mesh key = {'project'+i+'-physicsmesh'+it}
-                                    position = {new THREE.Vector3(pmesh.pos.x, pmesh.pos.y, pmesh.pos.z)} >
-                                {geo}
-                                <meshBasicMaterial color = {pmesh.color} key = {'project'+i+'physicsmtl'+it} transparent opacity = {0.4} />
-                            </mesh>
-                        )
-                    })
-                    }
-
-                    
-                </group>
+                <ProjectGroup 
+                    debug = {true}
+                    key = {project.name + 'Group'}
+                    project = {project}
+                    store = {physics}
+                    index = {i}
+                    onReady = {this.setReady}
+                />
             )
         })
 
@@ -354,7 +303,7 @@ import { degs, rads} from './helpers.js'
                 <perspectiveCamera 
                     name = "camera"
                     ref = "camera"
-                    fov = {canvas.fov}
+                    fov = {physics.fov}
                     aspect = {this.props.width/this.props.height}
                     near = {0.001}
                     far = {100}
@@ -426,7 +375,7 @@ import { degs, rads} from './helpers.js'
                 </group>
                 }
 
-                {projectMeshes}
+                {projectGroups}
 
                 </scene>
 
@@ -438,8 +387,8 @@ import { degs, rads} from './helpers.js'
 
 const debug = new Debug()
 window.debug = debug
-const canvas = new ThreePhysicsStore()
-window.world = canvas
+const physics = new ThreePhysicsStore()
+window.world = physics
 
 ThreeOimoTest.defaultProps = {
     debugModels: true
