8929f64ea5085a35009bcd5a933cd8d33d3d9ac8
Duck Leng <jack.leng@gmail.com>
2017-08-12 00:21:34 -0700

    added planes for bottom/sidebar

--- src/Projects/Sendbloom/Sidebar.jsx
+++ src/Projects/Sendbloom/Sidebar.jsx
@@ -8,14 +8,17 @@ export default class Sidebar extends React.Component{
         const windowcolor = {front: 0xededed, left:0xccd2d6, bottom: 0xededed, right: 0xededed, top: 0xfbfbfc, back: 0xededed}
         
         makeColorBox('sendbloom', this.refs.sidebar, [0.35, 0.925, 0.075], windowcolor)
-        this.refs.sidebar.position.set(-1,0,0.25)
-        this.refs.sidebar.visible = false
+        // this.refs.sidebar.position.set(-1,0,0.25)
+        this.reset()
+
     }
 
     reset(){
         const sidebar = this.refs.sidebar
+        this.refs.sidebar.visible = false
         sidebar.scale.set(0.3,1,1)
-        sidebar.position.set(-1,0,0.25)
+        sidebar.position.set(-.75,0,0.325)
+        this.refs.shadow.material.opacity = 0
     }
 
     cycleIn(){
@@ -28,12 +31,14 @@ export default class Sidebar extends React.Component{
         store.static = false
         // sidebar.visible = true
         this.sidebarTweens = [
-            twn('scale', {x: 0.3}, {x: 1}, 400, sidebar.scale, {delay: 200, onStart: ()=>{sidebar.visible=true}}),
-            twn('position', {x: -1, z: 0.25} ,{x: -0.55, z: 0.325}, 400, sidebar.position, {delay: 200}),
+            twn('scale', {x: 0.3}, {x: 1}, 250, sidebar.scale, {delay: 200, onStart: ()=>{sidebar.visible=true}}),
+            twn('position', {x: -0.77} ,{x: -0.55}, 250, sidebar.position, {delay: 200}),
             twn('opacity', {opacity:0}, {opacity:1}, 250, sidebar, 
                 {traverseOpacity: true, onComplete: ()=>{
                     store.bodies.sendbloom.allowSleep = true
-                }, delay: 200})
+                }, delay: 200}),
+            twn('opacity', {opacity: 0}, {opacity: 1}, 200, this.refs.shadow.material, {delay: 200}),
+            // twn('position', {x:-1})
         ]
 
     }
@@ -56,7 +61,16 @@ export default class Sidebar extends React.Component{
     }
     render(){
         return(
-            <group ref = "sidebar" />
+            <group ref = "sidebar">
+               
+                 <mesh ref = "shadow"
+                        // position = {v3(-0.55,0.075,0.085)}
+                        position = {v3(0,0.075,-.24)}
+                    >
+                    <planeBufferGeometry width = {0.425} height = {0.92} />
+                    <meshBasicMaterial color = {0x000000} transparent />
+                </mesh>
+            </group>
 
         )
     }
