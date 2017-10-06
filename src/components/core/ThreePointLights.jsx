e96d4417bbd5691b229338a871b26f1bb5d78b56
Jack L <jack.leng@gmail.com>
2017-10-06 03:10:23 -0700

    removed vestigial noncomponent threepointlights

--- src/components/core/ThreePointLights.jsx
+++ src/components/core/ThreePointLights.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import * as THREE from 'three'
 
-import {v3} from '../../helpers/utilities'
+import { v3 } from '../../helpers/utilities'
 
-export default class ThreePointLights extends React.Component{
-    render(){
-        const {light1, light2, light3} = this.props
-        return(
+export default class ThreePointLights extends React.Component {
+    render() {
+        const sf = this.props.scale
+        const { light1, light2, light3 } = this.props
+        return (
             <group>
                 <pointLight
-                    position={v3(light1.position.x, light1.position.y, light1.position.z)}
+                    position={v3(
+                        light1.position.x * sf,
+                        light1.position.y * sf,
+                        light1.position.z * sf
+                    )}
                     color={light1.color}
                     decay={3}
                     distance={36}
@@ -17,7 +22,11 @@ export default class ThreePointLights extends React.Component{
                     intensity={light1.intensity}
                 />
                 <pointLight
-                    position={v3(light2.position.x, light2.position.y, light2.position.z)}
+                    position={v3(
+                        light2.position.x * sf,
+                        light2.position.y * sf,
+                        light2.position.z * sf
+                    )}
                     color={light2.color}
                     decay={3}
                     distance={36}
@@ -25,35 +34,40 @@ export default class ThreePointLights extends React.Component{
                     intensity={light2.intensity}
                 />
                 <pointLight
-                    position={v3(light3.position.x, light3.position.y, light3.position.z)}
+                    position={v3(
+                        light3.position.x * sf,
+                        light3.position.y * sf,
+                        light3.position.z * sf
+                    )}
                     color={light3.color}
                     decay={3}
                     distance={36}
                     visible={light3.on}
                     intensity={light3.intensity}
                 />
-            </group>    
+            </group>
         )
     }
 }
 
 ThreePointLights.defaultProps = {
+    scale: 2,
     light1: {
-        position: v3(3,2,3.5),
+        position: v3(3, 2, 3.5),
         intensity: 0.6,
         color: 0xff0000,
-        on: true
+        on: true,
     },
     light2: {
-        position: v3(-5,-1,1.5),
+        position: v3(-5, -1, 1.5),
         intensity: 0.35,
         color: 0xffff00,
-        on: true
+        on: true,
     },
     light3: {
-        position: v3(0,3,-.5),
+        position: v3(0, 3, -0.5),
         intensity: 0.7,
         color: 0x0077ff,
-        on: true
-    }
+        on: true,
+    },
 }
